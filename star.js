715a916d7243472bedf6459577d37f7dd978179d
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-10-04 17:47:30 -0600

    Update and rename index.js to star.js

---
+++ star.js
@@ -0,0 +1,199 @@
+ /*//////////////////////////////////////////////////////////////////////////////////////
+ * Code by Tyler. H#9393!!
+ * Ninja Starboard Bot
+ *
+ *  I DO NOT authorize you to use this bot or the corresponding code without my consent first violation in this term will result in a copyright strike or lawsuit
+ *  If you fork this Bot repo make sure you give me credit for all files and folders associated as all the branding for this bot is registered and copy written. 
+ *  Due to the fact that no license is provided with this bot that means that it falls under the not publicly available category.
+ //////////////////////////////////////////////////////////////////////////////////////*/
+
+//////////////////////////////***IMPORT AND EXPORT MODULES***////////////////////////////////////////////////
+//////////////////////////////***TO AND FROM DATABASE/BANK***////////////////////////////////////////////////
+/*
+This imports all the folders and files for the required node modules from the bots database to be read (DO NOT touch these)
+//These import options will be disabled untill MySQL support is fully implemented, Enabling these without DB support will throw errors.
+//import Discord = require('discord.js');
+//import Music = require('discord.js-musicbot-addon');
+//import Enmap = require("enmap");
+//import fs = require("fs");
+//import snekfetch = require('snekfetch');
+//import talkedRecently = new Set();
+//import token = process.env.BOT_TOKEN; //Replace with config.token if self hosting and define the token in the config. 
+//import client = new Discord.Client();
+//import config = require('./data/config.json');
+//import adlinks = require('./src/blacklist/blacklistedlinks.json');
+//import offensivewords = require('./src/blacklist/blacklistedwords.json');
+//import profanities = require('./src/blacklist/blacklistedswears.json');
+//import antispam = require('discord-anti-spam');
+//import chalk = require('chalk');
+//import mysql = require('mysql');
+//import mongoose = require('mongoose');
+//import spamDetector = require('./anti_spam.js');
+//import Canvas = require('canvas');
+//import { Kayn, REGIONS } = require('kayn');
+//import mysql = require('mysql');
+//import file = require('../mysql.json');
+//import { CommandoClient } = require('./commando')
+//import jimp = require('jimp');
+*/
+
+//////////////////////////////***REQUIRED NODE MODULES***////////////////////////////////////////////////
+/* This is where the bot reads the required node modules, DO NOT touch these. */
+//const mysql = require('mysql');
+//const mongoose = require('mongoose');
+//const spamDetector = require('./anti_spam.js');
+//const { Kayn, REGIONS } = require('kayn');
+//const mysql = require('mysql');
+//const file = require('../mysql.json');
+//const { CommandoClient } = require('./commando')
+//const jimp = require('jimp');
+//const blacklist = require("./data/serverfilters.js");
+const Discord = require("discord.js");
+const Music = require('discord.js-musicbot-addon');
+const Enmap = require("enmap");
+const fs = require("fs");
+const moment = require("moment"); 
+const snekfetch = require('snekfetch');
+const talkedRecently = new Set();
+const token = process.env.BOT_TOKEN;
+const prefix = process.env.PREFIX;
+const prefix2 = require('./data/config.json');
+const prefix3 = require('./data/config.json');
+const prefix4 = require('./data/config.json');
+const InvPerm = require('./data/config.json');
+const star = new Discord.Client();
+const starLog
+const config = require('./data/config.json');
+const adlinks = require('./src/blacklist/blacklistedlinks.json');
+const offensivewords = require('./src/blacklist/blacklistedwords.json');
+const profanities = require('./src/blacklist/blacklistedswears.json');
+const antispam = require('discord-anti-spam');
+const chalk = require('chalk');
+const Canvas = require('canvas');
+const dbots = require('superagent')
+const timers = require('timers')
+const activity1 = config.activity1
+const type1 = config.type1
+const activity2 = config.activity2
+const type2 = config.type2
+const activity3 = config.activity3
+const type3 = config.type3
+const type4 = config.type4
+const activity4 = config.activity4
+const time1 = config.activitytime
+client.music = require("discord.js-musicbot-addon");
+const musicCommands = require("./src/music.json");
+
+//////////////////////////////***PLAYING STATUS, INVITE TIMEOUTS & MORE***////////////////////////////////////////
+  star.on('ready',  async () => {
+let activities = [
+  {
+    name:`${activity1} in ${star.guilds.size} Servers`,
+     options:{
+      type:`${type1}`
+    }
+  },
+  {
+    name:`${activity2} for ${star.users.size} Users`,
+     options:{
+      type:`${type2}`
+    }
+  }
+]
+let i = 0;
+
+    starLog(`${star.user.username} has started, with ${star.users.size} users, in ${star.channels.size} channels of ${star.guilds.size} guilds.`);
+    timers.setInterval(() => {
+      i = i == activities.length ? 0 : i
+      client.user.setActivity(activities[i].name, activities[i].options)
+      i++
+    }, time1)
+  });
+
+  // "ready" isn't really ready. We need to wait a spell.
+  wait(1000); 
+
+client.on("guildCreate", guild => {
+  // This event triggers when the bot joins a guild.
+let gcEmbed = new Discord.RichEmbed()
+ .setTitle("Thanks For Adding Me")
+ .setColor("#0x3dfbff")
+ .setTimestamp()
+ .setDescription("***Please Note:** Ninja Bot requires various channels to complete proper setup")
+ .addField("Setup Help", "``nb/setup`` Shows you how to setup Ninja Bot") 
+ .setFooter("Ninja Bot")
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setGame(`gen help in ${client.guilds.size} Servers`, `https://www.twitch.tv/monstercat`);
+  guild.owner.send(gcEmbed)
+ });
+
+//////////////////////////////***MESSAGE DELETE FUNCTIONS***/////////////////////////////////////////////
+/*This event allows any message that the bot reacts to with the emoji provided,
+to be deleted when the reaction is pressed by ANY user (Not admin only)*/
+star.on('messageReactionAdd', (reaction, user) => {
+    if (user.bot) return;
+    if (reaction.emoji.name === "🆗") {
+    reaction.message.delete();
+    }
+})
+
+//////////////////////////////***EVENT COLLECTION FUNCTION***////////////////////////////////////////////////
+//Allows the bot to log and show events (Joining New Servers)
+fs.readdir("./events/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    const event = require(`./events/${file}`);
+    let eventName = file.split(".")[0];
+    client.on(eventName, event.bind(null, client));
+  });
+});
+
+star.commands = new Enmap();
+star.aliases = new Enmap();
+
+//Stops the bot from responding to other bots.
+star.on('message', message => {
+  if(message.author.bot) return;
+})
+
+//////////////////////////////***DEFINE COMMANDS FUNCTION***////////////////////////////////////////////////
+//client.commands = new Discord.Collection(); // Collection for all commands
+//client.aliases = new Discord.Collection(); // Collection for all aliases of every ccommand
+
+//This code line allows the commands to be individual/seperate files
+fs.readdir("./commands/", (err, files) => {
+  if (err) return console.error(err);
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    starLog(`Attempting to load command ${commandName}`);
+    star.commands.set(commandName, props);
+  });
+});
+
+//////////////////////////////***DEFINE COMMANDS FUNCTION***////////////////////////////////////////////////
+const modules = ['Help']; // This will be the list of the names of all modules (folder) your bot oown
+
+modules.forEach(c => {
+fs.readdir(`./src/commands/${c}/`, (err, files) => { // Here we go through all folders (modules)
+ if (err) throw err; // If there is error, throw an error in the console
+  starLog(`[Commandlogs] Loaded ${files.length} commands of module ${c}`); // When commands of a module are successfully loaded, you can see it in the console
+   files.forEach(f => { // Now we go through all files of a folder (module)
+    const props = require(`./src/commands/${c}/${f}`); // Location of the current command file
+     star.commands.set(props.help.name, props); // Now we add the commmand in the client.commands Collection which we defined in previous code
+      props.conf.aliases.forEach(alias => { // It could be that the command has aliases, so we go through them too
+       star.aliases.set(alias, props.help.name); // If we find one, we add it to the client.aliases Collection
+       });
+    });
+ });
+
+});
+star.login(process.env.BOT_TOKEN); //process.env.BOT_TOKEN Allows the token to be defined and set via the bots database to ensure it is never public!
+
+/* Code by Tyler. H#9393!!
+ *  I DO NOT authorize you to use this bot or the corresponding code without my consent first.
+ *  If you fork this Bot repo make sure you give me credit for all files and folders associated. 
+ *  Due to the fact that no license is provided with this bot that means that it falls under the not publicly available category.
+ * Unauthorized use of this bot could result in your GitHub and Discord accounts being terminated!
+ */
