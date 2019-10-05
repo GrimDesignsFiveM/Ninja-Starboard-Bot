9aa5cf1076385b75cb5e6f5726363f2575a635a6
ツThe Watchers Bot Devsツ <47341267+GrimDesignsFiveM@users.noreply.github.com>
2019-10-04 23:11:44 -0600

    Update messageReactionAdd.js

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -28,7 +28,7 @@ async function starEmbed(color, description, author, thumbnail, timestamp, foote
     return embed;
 }
 
-exports.run = async (client, message, args) => {
+exports.run = async (client, args) => {
 const embed2 = new Discord.RichEmbed()
 
      embed2.setTitle("Ninja Starboard Channel")
